88bddcf7dd44d2ac36d14c9e5aceb0aa57908190
Haoyan Li <angelgamer@gmail.com>
2016-07-10 04:43:45 +0900

    Create new source code of serial communication; Create a test code for serial communication; Create a log class; Add a UDP server in udpclient.py

---
+++ src/serialtest.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import serial
+import threading
+import time
+import random
+import loginstance
+
+HEX_DICT = [
+    "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+    "A", "B", "C", "D", "E", "F"
+]
+
+
+class SerialCommunicationThread(threading.Thread):
+
+    def __init__(self, port, sending_interval):
+        super(SerialCommunicationThread, self).__init__()
+        self.ser = serial.Serial(port, baudrate=57600)
+        self.sending_interval = sending_interval / 1000.0
+        self.stop_flag = True
+        self.logger = loginstance.Logger(
+            logname=u"log.txt",
+            loglevel=1,
+            logger=u"SerialInfoLog").getlog()
+
+        self.ser.flushInput()
+
+    def open(self):
+        try:
+            self.ser.open()
+        except (OSError, serial.SerialException):
+            self.ser.close()
+            self.ser.open()
+
+        self.stop_flag = False
+
+    def close(self):
+        self.stop_flag = True
+        time.sleep(1)
+        self.ser.close()
+
+    def receive_data(self):
+        # while self.ser.in_waiting != 0:
+        #     pass
+        #
+        # time.sleep(1)
+        rev_data = self.ser.read(self.ser.in_waiting)
+        self.logger.info(u"Receive: " + rev_data[:-1])
+
+    def send_msg(self, msg):
+        self.ser.write(msg.encode(u"utf-8"))
+        self.logger.info(u"Send: " + msg[:-2])
+
+    def _generate_random_data(self, bits):
+        seq = []
+        for idx in range(bits):
+            seq.append(random.choice(HEX_DICT))
+        # print seq
+        # print u"0x" + u"".join(seq)
+        msg = u"N  OK   0x0B06 VALUE:0x" + u"".join(seq) + u"\r\n"
+        self.logger.info(u"Send: Data - " + msg[:-2])
+        return msg
+
+    def run(self):
+        while not self.stop_flag:
+            self.ser.write(self._generate_random_data(40).encode())
+            time.sleep(self.sending_interval)
+        pass
+
+# N  OK   0x0B06 VALUE:0xF0FF44DA030064FD01FF0BFC6600EBFF87FF88C7
+
+
+if __name__ == '__main__':
+    mode = raw_input(u"Please input mode: ")
+    if int(mode) == 1:
+        msg = u"1 0xE9A8F13D5E7C -070 LAPIS_RAW1 \r\n"
+        port = u"COM3"
+    else:
+        msg = u"1 0xF5A8F13D5E7C -070 LAPIS_RAW0 \r\n"
+        port = u"COM5"
+
+    thread = SerialCommunicationThread(port, 100)
+    thread.open()
+
+    raw_input(u"Waiting...")
+    thread.receive_data()
+    thread.send_msg(msg)
+
+    raw_input(u"Waiting...")
+    thread.receive_data()
+    thread.send_msg(u"CONNECT\r\n")
+
+    raw_input(u"Waiting...")
+    thread.receive_data()
+    thread.send_msg(u"W  OK   0x0B07\n" +
+                    u"OK\r\n")
+
+    thread.start()
+    raw_input()
+    # print thread.isAlive()
+
+    thread.close()
--- src/udpclient.py
+++ src/udpclient.py
@@ -3,42 +3,64 @@
 
 import socket
 import json
+import loginstance
 
 FILE_NAME = u"config.json"
 MESSAGE = "Hello, World!"
 
 
-def load_config():
-    """ Loading a pre-defined configuration file.
+# def load_config():
+#     """ Loading a pre-defined configuration file.
+#
+#     :return: configuration values[Dict]
+#     """
+#     with open(FILE_NAME) as f:
+#         config = json.load(f)
+#
+#     return config
 
-    :return: configuration values[Dict]
-    """
-    with open(FILE_NAME) as f:
-        config = json.load(f)
 
-    return config
-
-
-class UdpSender:
-    def __init__(self, udp_ip, udp_port):
+class UdpClient:
+    def __init__(self):
         self.socket = socket.socket(socket.AF_INET,
                                     socket.SOCK_DGRAM)
-        self.udp_ip = udp_ip
-        self.udp_port = udp_port
+        self._load_config()
+        self.udp_ip = self.config[u"IP_Address"]
+        self.udp_port = self.config[u"Port"]
 
     def send_msg(self, msg):
         self.socket.sendto(msg, (self.udp_ip, self.udp_port))
 
+    def _load_config(self):
+        with open(FILE_NAME) as f:
+            self.config = json.load(f)
+
 
 if __name__ == '__main__':
-    cfg = load_config()
-    print cfg
-    ip = cfg[u"IP_Address"]
-    port = cfg[u"Port"]
+    # cfg = load_config()
+    # print cfg
+    # ip = cfg[u"IP_Address"]
+    # port = cfg[u"Port"]
 
-    print "UDP target IP:", ip
-    print "UDP target port:", port
-    print "message:", MESSAGE
+    # print "UDP target IP:", ip
+    # print "UDP target port:", port
+    # print "message:", MESSAGE
 
-    udp_sender = UdpSender(ip, port)
-    udp_sender.send_msg(MESSAGE)
+    # udp_sender = UdpClient()
+    # udp_sender.send_msg(MESSAGE)
+    logger = loginstance.Logger(
+        logname=u"udp_log",
+        loglevel=1,
+        logger=u"UDPServerLog").getlog()
+
+    UDP_IP = "127.0.0.1"
+    UDP_PORT = 4444
+
+    sock = socket.socket(socket.AF_INET,  # Internet
+                         socket.SOCK_DGRAM)  # UDP
+    sock.bind((UDP_IP, UDP_PORT))
+
+    while True:
+        data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
+        logger.info(data)
+        # print "received message:", data
